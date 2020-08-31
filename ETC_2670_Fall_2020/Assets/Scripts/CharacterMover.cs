0b73c7bad954c55d50b4fbb2b4532fd704a17a6f
Ericio <eric322000@gmail.com>
2020-08-31 14:11:50 -0600

    CharacterMove
    Added character movement

---
+++ ETC_2670_Fall_2020/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMover : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 movement;
+    public float gravity = -9.81f;
+    public float moveSpeed = 3f;
+    public float fastMoveSpeed;
+    public float jumpForce = 10f;
+    public int jumpCountMax;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal")*moveSpeed;
+       // To us the GetKey function git rid of Input.GetAxis("Horizontal")*
+     //   if (Input.GetKey(KeyCode.Y))
+     //   {
+     //       movement.x *= moveSpeed;
+     //   }
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            movement.y = jumpForce;
+        }
+
+        if (controller.isGrounded)
+        {
+            movement.y = 0;
+        }
+
+        else
+        {
+            movement.y -= gravity;
+        }
+        
+        controller.Move(movement*Time.deltaTime);
+    }
+}
