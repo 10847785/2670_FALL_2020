a25ffa88ce8757dc166b1a2da38e594d4f7002ba
Ericio <eric322000@gmail.com>
2020-09-16 14:17:38 -0600

    LookAt
    Able to make look at function, curser moves objects

---
+++ ETC_2670_Fall_2020/Assets/Scripts/MoveLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLocator : MonoBehaviour
+{
+    private Camera cam;
+    public Transform pointObj;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        //taken from source code, but modified, might need to check later
+        if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out var hit, 100))
+        {
+            pointObj.position = hit.point;
+        }
+    }
+}
