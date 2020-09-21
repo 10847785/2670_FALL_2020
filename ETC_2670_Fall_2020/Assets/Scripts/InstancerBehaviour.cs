7b33ad1454acc4adaa8a5912357af42431a89481
Ericio <eric322000@gmail.com>
2020-09-21 14:16:56 -0600

    Reset game field
    Making new game field

--- ETC_2670_Fall_2020/Assets/Scripts/InstancerBehaviour.cs
+++ ETC_2670_Fall_2020/Assets/Scripts/InstancerBehaviour.cs
@@ -2,16 +2,16 @@
 using UnityEngine;
 
 
-public class Instancer : MonoBehaviour
+public class InstancerBehaviour : MonoBehaviour
 {
     public GameObject prefab;
-    
+
+    public Vector3Data rotationDirection;
     //Make a method to call the Instance Method
 
     private void Instance()
     {
         var location = transform.position;
-        var rotationDirection = new Vector3(0f,45f,0f);
-        Instantiate(prefab,location, Quaternion.Euler(rotationDirection));
+        Instantiate(prefab,location, Quaternion.Euler(rotationDirection.value));
     }
 }
--- ETC_2670_Fall_2020/Assets/Scripts/MoveLocator.cs
+++ ETC_2670_Fall_2020/Assets/Scripts/MoveLocator.cs
@@ -13,7 +13,7 @@ public class MoveLocator : MonoBehaviour
         cam = Camera.main;
     }
 
-    private void Update()
+    public void Move()
     {
         //taken from source code, but modified, might need to check later
         if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out var hit, 100))
