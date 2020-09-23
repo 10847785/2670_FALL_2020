43cea1fec98bdcd91d1d19961263e0ddcec2df10
Ericio <eric322000@gmail.com>
2020-09-23 14:11:03 -0600

    Rearranging Scripts
    Making new code for scripts

---
+++ ETC_2670_Fall_2020/Assets/Scripts/Behaviours/MouseEventsBehaviour1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+public class MouseEventsBehaviour1 : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent;
+
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+}
---
+++ ETC_2670_Fall_2020/Assets/Scripts/ScriptableObjects/Debugger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Debugger : ScriptableObject
+{
+    public void OnDebug(string obj)
+    {
+        Debug.Log(obj);
+    }
+}
