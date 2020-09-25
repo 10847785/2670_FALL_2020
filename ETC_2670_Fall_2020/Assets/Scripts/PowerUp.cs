1414d180b4c2d7e1e09f746b9d28ef05c98c6d14
Ericio <eric322000@gmail.com>
2020-09-25 15:36:22 -0600

    Improve
    More improvments

---
+++ ETC_2670_Fall_2020/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public FloatData powerData;
+    public float powerUpLevel = 0.1f;
+
+    private void OnParticleTrigger(Collider other)
+    {
+        powerData.value += powerUpLevel;
+    }
+}
