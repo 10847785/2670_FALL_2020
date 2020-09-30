64047a30f3e36fdb651457525d8c8dbd229191c7
Ericio <eric322000@gmail.com>
2020-09-30 14:14:02 -0600

    KnockBack
    Added knock back function

---
+++ ETC_2670_Fall_2020/Assets/Scripts/Behaviours/CharacterKnockBack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class CharacterKnockBack : MonoBehaviour
+{
+    //Direction of the hit
+    
+    public CharacterController controller;
+    public Vector3 knockBackVector;
+    public float knockBackForce = 50f;
+    private float tempForce;
+    private void Start()
+    {
+        tempForce = knockBackForce;
+    }
+    
+    private IEnumerator OnTriggerEnter()
+    {
+        while (knockBackForce > 0)
+        {
+            knockBackVector.x = knockBackForce * Time.deltaTime;
+            controller.Move(knockBackVector);
+            knockBackForce -= 0.1f;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
