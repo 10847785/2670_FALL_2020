79c60d16796d94f56d5373f7d5bed9dddb766084
Ericio <eric322000@gmail.com>
2020-10-01 23:12:32 -0600

    Worked on level design
    Worked on the design of the level

---
+++ Run/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Vector3 = UnityEngine.Vector3;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 20;
+    public float rotateSpeed = 30f;
+    private Rigidbody rbody;
+    public float normalSpeed, fastSpeed;
+    private Vector3 movement;
+   
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        var horAxis = Input.GetAxis("Horizontal")*Time.deltaTime*rotateSpeed;
+        transform.Rotate(0,horAxis,0);
+        var vertAxis = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(horAxis, 0, vertAxis) * moveSpeed * Time.deltaTime;
+        
+        rbody.MovePosition(transform.position + movement);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveSpeed = 50;
+        }
+
+        else
+        {
+            moveSpeed = 20;
+        }
+       
+    }
+}
