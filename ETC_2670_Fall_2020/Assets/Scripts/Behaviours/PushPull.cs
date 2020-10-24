10f93a478724503db8fe29b676f3eadfbab7ef7b
Ericio <eric322000@gmail.com>
2020-10-24 12:34:56 -0600

    Text

---
+++ ETC_2670_Fall_2020/Assets/Scripts/Behaviours/PushPull.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushPull : MonoBehaviour
+{
+    public float push = 10f;
+    void Update()
+    {
+        if (Input.GetKeyDown(0))
+        {
+            GetComponent<Rigidbody>().AddForce(force: transform.forward * -1f * push);
+        }
+    }
+    
+}
