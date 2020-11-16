4f6b7e924cc55949b45e749afab7075b67f9ce64
Ericio <eric322000@gmail.com>
2020-11-16 14:12:57 -0700

    More Brains

---
+++ ETC_2670_Fall_2020/Assets/Scripts/AI/AIBrainBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu]
+public class AIBrainBase : ScriptableObject
+{
+    public float health;
+    public float speed;
+    public Mesh art;
+
+    public virtual void Navigate(NavMeshAgent agent)
+    {
+        //destination
+        //Make agents do different things
+    }
+    
+}
---
+++ ETC_2670_Fall_2020/Assets/Scripts/AI/AIWithBrains.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+//[RequireComponent(typeof())]
+public class AIWithBrains : MonoBehaviour
+{
+    public AIBrainBase aiBrain;
+    public Transform player;
+    private int i;
+    public List<AIBrainBase> brainUpgrades;
+
+    private MeshFilter filter;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        filter = GetComponent<MeshFilter>();
+        filter.mesh = aiBrain.art;
+    }
+
+    private void Update()
+    {
+        aiBrain.Navigate(agent);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        aiBrain = brainUpgrades[i];
+        filter.mesh = aiBrain.art;
+
+        if (i < brainUpgrades.Count)
+        {
+            i++;
+        }
+    }
+}
