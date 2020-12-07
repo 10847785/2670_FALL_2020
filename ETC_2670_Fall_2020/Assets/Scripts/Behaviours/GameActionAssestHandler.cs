be28fcc4fcf3a18c8fe879fccc53c52acc0c4d46
Ericio <eric322000@gmail.com>
2020-12-07 16:19:32 -0700

    Intro

---
+++ ETC_2670_Fall_2020/Assets/Scripts/Behaviours/GameActionAssestHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class GameActionAssestHandler : MonoBehaviour
+{
+    public GameAction gameActionObj;
+    public UnityEvent handleEvent;
+
+    private void OnEnable()
+    {
+        gameActionObj.action += ActionHandler;
+    }
+}
